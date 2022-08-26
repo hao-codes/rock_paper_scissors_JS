22e90e708e88232ae6d1e32ddd85d9fe99238d1a
hao-codes <59805422+hao-codes@users.noreply.github.com>
2022-08-26 22:46:53 +0200

    make gamebuttons work for 3 options

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,26 +1,14 @@
 // possible options for rps
 let options = ["rock", "paper", "scissors"];
 
-// function to get choice of computer
+// function to get random choice of computer
 function getComputerChoice() {
 
     return options[Math.floor(Math.random() * options.length)];
 
 }
 
-// function getMyChoice() {
-//     let playerSelection = prompt("Rock, Paper or Scissors?");
-//     playerSelection = playerSelection.toLowerCase()
-//     console.log(playerSelection);
-//     while (options.includes(playerSelection) == false) {
-//         let playerSelection = prompt("Invalid input, choose Rock, Paper or Scissors?");
-//         playerSelection = playerSelection.toLowerCase();
-//         if (options.includes(playerSelection) == true) {
-//             break
-//         }
-//     }
-//     return playerSelection;
-// }
+
 
 //game logic for one round, returns 
 function playRound(playerSelection, computerSelection) {
@@ -38,13 +26,7 @@ function playRound(playerSelection, computerSelection) {
 
     }
 }
-//playerSelection = getMyChoice();
 
-
-
-// function clickHandler(event) {
-//     console.log('Button Clicked');
-// }
 
 // set scores at start of game
 
@@ -63,27 +45,85 @@ computerScore.innerHTML = intComputerScore;
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", function(e) {
     //console.log(e.srcElement.id);
+    if (roundCounter == 0) {
+        document.getElementById("endGame").innerText = "";
 
+        intPlayerScore = 0;
+        intComputerScore = 0;
+        playerScore.textContent = intPlayerScore;
+        computerScore.textContent = intComputerScore;
+    }
     let result = playRound(e.srcElement.id, getComputerChoice());
     document.getElementById("roundResult").innerText = result;
     console.log(result);
-});
+    if (result[4] == "w") {
+        intPlayerScore += 1;
+        playerScore.innerHTML = (intPlayerScore);
+    } else {
+        intComputerScore += 1;
+        computerScore.innerHTML = (intComputerScore);
+    }
+    // console logs
+    roundCounter += 1;
+
+    console.log(intPlayerScore, intComputerScore,result);
+    
+    //check if player/computer has won 5 rounds
+    if (intComputerScore >= 5 || intPlayerScore >= 5) {
+        playerScore.innerHTML = (intPlayerScore);
+        computerScore.innerHTML = (intComputerScore);
+        newGameButton();
+        //scissors.disabled = true;
+        console.log("new game", roundCounter);
+        //newGameButton();
+
+}});
 
 const paper = document.querySelector("#paper");
 paper.addEventListener("click", function(e) {
     //console.log(e.srcElement.id);
+    if (roundCounter == 0) {
+        document.getElementById("endGame").innerText = "";
 
+        intPlayerScore = 0;
+        intComputerScore = 0;
+        playerScore.textContent = intPlayerScore;
+        computerScore.textContent = intComputerScore;
+    }
     let result = playRound(e.srcElement.id, getComputerChoice());
     document.getElementById("roundResult").innerText = result;
-    console.log(result);
-});
+    //console.log(result);
+    if (result[4] == "w") {
+        intPlayerScore += 1;
+        playerScore.innerHTML = (intPlayerScore);
+    } else {
+        intComputerScore += 1;
+        computerScore.innerHTML = (intComputerScore);
+    }
+    // console logs
+    roundCounter += 1;
+
+    console.log(intPlayerScore, result);
+    
+    //check if player/computer has won 5 rounds
+    if (intComputerScore >= 5 || intPlayerScore >= 5) {
+        playerScore.innerHTML = (intPlayerScore);
+        computerScore.innerHTML = (intComputerScore);
+        newGameButton();
+        //scissors.disabled = true;
+        console.log("new game", roundCounter);
+        //newGameButton();
+
+}});
 
 let roundCounter = 0;
 const scissors = document.querySelector("#scissors");
 scissors.addEventListener("click", function(e) {
     //console.log(e.srcElement.id);
+
     if (roundCounter == 0) {
         document.getElementById("endGame").innerText = "";
+
         intPlayerScore = 0;
         intComputerScore = 0;
         playerScore.textContent = intPlayerScore;
@@ -93,29 +133,36 @@ scissors.addEventListener("click", function(e) {
     document.getElementById("roundResult").innerText = result;
 
     if (result[4] == "w") {
-        //intPlayerScore += 1;
-        playerScore.innerHTML = (intPlayerScore += 1);
+        intPlayerScore += 1;
+        playerScore.innerHTML = (intPlayerScore);
     } else {
-        //intComputerScore += 1;
-        computerScore.innerHTML = (intComputerScore += 1);
+        intComputerScore += 1;
+        computerScore.innerHTML = (intComputerScore);
     }
-
+    // console logs
     roundCounter += 1;
-    console.log("-----", roundCounter);
-    console.log(intPlayerScore, intComputerScore);
-    console.log(result);
+
+    console.log(intPlayerScore, intComputerScore, result);
+
+    //check if player/computer has won 5 rounds
     if (intComputerScore >= 5 || intPlayerScore >= 5) {
-        document.getElementById("endGame").innerHTML = "<button>Game Ended</button>";
-        roundCounter = 0;
+        playerScore.innerHTML = (intPlayerScore);
+        computerScore.innerHTML = (intComputerScore);
+        newGameButton();
+        //scissors.disabled = true;
         console.log("new game", roundCounter);
         //newGameButton();
+
     };
 });
 
 //const gameEnd = document.querySelector("#gameEnd");
-// function newGameButton() {
-//     document.getElementById("endGame").textContent = ' Play again ';
-// }
+function newGameButton() {
+    // document.getElementById("endGame").innerHTML = "<button>Game Ended</button>";
+    endGame.innerHTML = "<button>Play again</button>";
+    roundCounter = 0;
+    scissors.disabled = false;
+}
 
 const endGame = document.querySelector("#endGame");
 endGame.addEventListener("click", function(e) {
@@ -126,33 +173,3 @@ endGame.addEventListener("click", function(e) {
     computerScore.textContent = 0;
     endGame.innerHTML = "";
 })
-
-
-// game for running on console
-function game() {
-    // console.log("Let's play 5 rounds of Rock, Paper, Scissors :D")
-    let myWins = 0;
-    let pcWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getMyChoice();
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result[4] == "w") {
-            myWins += 1;
-        } else {
-            pcWins += 1;
-        }
-        console.log(result);
-    }
-    console.log("Results You: " + myWins.toString());
-    console.log("Results  PC: " + pcWins.toString());
-    if (myWins == pcWins) {
-        console.log("Game is tied");
-    } else if (myWins > pcWins) {
-        console.log("Congrats, you won the total game.");
-    } else {
-        console.log("You lost the total game, better luck next time");
-
-    }
-
-}
